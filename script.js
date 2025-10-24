119cd2c71a184d60d14b0793e23a7932facb5cc9
setifeliott@gmail.com <setifeliott@gmail.com>
2025-10-24 17:45:58 +0200

    Ajout de l'image dynamique et modification des boutons en fonction de l'id et non index en JS. + Modification CSS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('Ça marche 🚀');
 const container = document.getElementById("btn-container");
+const elFinText = document.getElementById("finText");
 
 // ---------- VOTRE JS ICI ----------
 let para = document.getElementById('para');
@@ -8,35 +9,50 @@ let monId = 1;
 
 //Fonction pour creer les boutons dynamiquement
 function getBtnDOM() {
+
     //on vide le container 
     container.innerHTML = "";
 
+    //recherche dans mon tab l'id qui correspond
     const question = tableauQuestions.find(_ => _.id === monId);
+
+    //on recupère le texte pour le mettre a l'endroit du HTML
     para.textContent = question.text;
-    //Pour chaque index du array 'answers'
-    tableauQuestions[monId -1 ].answers.forEach(answer => {
+
+    //on récupère l'image et on modifie le backgroundImage
+    document.querySelector('.story-img').style.backgroundImage = `url('${question.image}')`;
+
+    //Pour chaque element du array 'answers' en fonction de l'id souhaité 
+     question.answers.forEach(answer => {
+
         //on creer notre bouton dynamique
         const elBtn = document.createElement("button");
+
         //on ajoute sa classe
         elBtn.classList.add('btn');
+
         //son contenu 
         elBtn.textContent = answer.text;
+
         //l'endroit de son parent
         container.appendChild(elBtn);
 
+        //Ecouteur sur les boutons
         elBtn.addEventListener('click', function () {
             monId = answer.nextId;
             console.log(monId);
-            
-        
 
+            if (monId === 0){
+                const fin = tableauFins.find(_ => _.id === answers.moralId);
+                elFinText.textContent = fin.text;
+            }
             handlerBtnClick();
         })
     })
 }
 
 //Focntion pour la gestion apres le click
-function handlerBtnClick() {
+function checkEnding() {
     //TODO: Si le id == 0 
     //add la classe hidden a la modale
     //clear le innerHTML
