cc06585d4961db1a6ec74c1489f5c75cc55efe1b
setifeliott@gmail.com <setifeliott@gmail.com>
2025-10-24 21:49:51 +0200

    Refonte totale CSS, Ajustement HTML, Modif const JS et ajout des commantaires

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 console.log('Ça marche 🚀');
 
-const container = document.getElementById("btn-container");
-const elFinText = document.getElementById("finText");
-const elModal = document.getElementById("modal-win");
+
+const elPara = document.getElementById('para');
 const elBtnPlayAgain = document.getElementById('btn-play-again');
+const elModal = document.getElementById("modal-win");
+const elFinText = document.getElementById("fin-text");
+const elBackBtn = document.getElementById("back-btn");
+
 
 
 // ---------- VOTRE JS ICI ----------
-let para = document.getElementById('para');
 let monId = 1;
 
 //Ecouteur sur elBtnPlayAgain
 elBtnPlayAgain.addEventListener('click', function() {
     //on cache la modale
     elModal.classList.add('hidden');
+
+    //on réinitialise l'id 
     monId = 1;
+    
     getBtnDOM();
 })
 
 //Fonction pour creer les boutons dynamiquement
 function getBtnDOM() {
 
-    
-
     //on vide le container 
-    container.innerHTML = "";
+    elBackBtn.innerHTML = "";
 
     //recherche dans mon tab l'id qui correspond
     const question = tableauQuestions.find(_ => _.id === monId);
 
     //on recupère le texte pour le mettre a l'endroit du HTML
-    para.textContent = question.text;
+    elPara.textContent = question.text;
 
     //on récupère l'image et on modifie le backgroundImage
-    document.querySelector('.story-img').style.backgroundImage = `url('${question.image}')`;
+    document.querySelector('.img').style.backgroundImage = `url('${question.image}')`;
 
     //Pour chaque element du array 'answers' en fonction de l'id souhaité 
      question.answers.forEach(answer => {
@@ -48,16 +51,22 @@ function getBtnDOM() {
         elBtn.textContent = answer.text;
 
         //l'endroit de son parent
-        container.appendChild(elBtn);
+        elBackBtn.appendChild(elBtn);
 
         //Ecouteur sur les boutons
         elBtn.addEventListener('click', function () {
             monId = answer.nextId;
-            console.log(monId);
+            //console.log(monId);
 
+            //Verification qu'on atteint une fin car id =0
             if (monId === 0){
+                //recherche dans mon tab de fin l'id correspondant au moralId
                 const fin = tableauFins.find(_ => _.id === answer.moralId);
+
+                //on affiche la modale
                 elModal.classList.remove('hidden');
+                
+                //on récupere le text a cet id
                 elFinText.textContent = fin.text;
             }
             getBtnDOM();
